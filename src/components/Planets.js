b110e36f20851eae7aface3ac8e37c13a42e1fe5
Ramon Bezerra <ramonbezerra90@gmail.com>
2020-07-29 11:15:04 -0300

    including query variables

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,33 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
 import Planet from "./Planet";
 
-const fetchPlanets = async () => {
-  const res = await fetch("http://swapi.dev/api/planets");
+const fetchPlanets = async (key, greeting, page) => {
+  console.log(greeting);
+  const res = await fetch(`http://swapi.dev/api/planets/?page=${page}`);
   return res.json();
 };
 
 const Planets = () => {
-  const { data, status } = useQuery("planets", fetchPlanets, {
-    staleTime: 0,
-    // cacheTime: 10,
-    onSuccess: () => console.log('data fetched with no problem')
-  });
+  const [page, setPage] = useState(1);
+  const { data, status } = useQuery(
+    ["planets", "hello, ninjas", page],
+    fetchPlanets,
+    {
+      staleTime: 0,
+      // cacheTime: 10,
+      onSuccess: () => console.log("data fetched with no problem"),
+    }
+  );
 
   console.log(data, status);
 
   return (
     <div>
       <h2>Planets</h2>
-      {status === 'loading' && (
-        <div>loading data...</div>
-      )}
-      {status === 'error' && (
-        <div>Error fetching data</div>
-      )}
-      {status === 'success' && (
+      <button onClick={() => setPage(1)}>page 1</button>
+      <button onClick={() => setPage(2)}>page 2</button>
+      <button onClick={() => setPage(3)}>page 3</button>
+
+      {status === "loading" && <div>loading data...</div>}
+      {status === "error" && <div>Error fetching data</div>}
+      {status === "success" && (
         <div>
-          {data.results.map(planet => <Planet key={planet.name} planet={planet}/>)}
+          {data.results.map((planet) => (
+            <Planet key={planet.name} planet={planet} />
+          ))}
         </div>
       )}
     </div>
