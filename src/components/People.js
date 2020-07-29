6c373a6236f6972cccc127df38950d0387c33d68
Ramon Bezerra <ramonbezerra90@gmail.com>
2020-07-29 10:09:01 -0300

    first blood

---
+++ src/components/People.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const People = () => {
+  return <h2>People</h2>;
+};
+
+export default People;
---
+++ src/components/Planets.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Planets = () => {
+  return <h2>Planets</h2>;
+};
+
+export default Planets;
